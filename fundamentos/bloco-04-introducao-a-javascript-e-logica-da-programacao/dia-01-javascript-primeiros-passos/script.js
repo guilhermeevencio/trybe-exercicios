4822c2dd4e085bec81f41ec914e9d45770a23ba9
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-26 18:29:14 -0300

    adiciona o exercicio 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -24,3 +24,20 @@ else {
     console.log(`A e b são iguais!`);
 }*/
 
+//terceiro exercicio
+
+/*const a = 1
+const b = 2
+const c = 2
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else if (c > a && c > b) {
+    console.log (c);
+} else {
+    console.log(`eles sao iguais`)
+}*/
+
+
