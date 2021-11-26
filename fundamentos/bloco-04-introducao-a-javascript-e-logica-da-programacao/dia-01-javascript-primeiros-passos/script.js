45f8124385ba6203ca68560bd74a7b24e415d256
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-26 18:23:08 -0300

    adiciona o segundo exercicio

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -1,10 +1,26 @@
 //primeiro exercicio
 
-const a = 10
+/*const a = 10
 const b = 2
 console.log (a + b)
 console.log (a - b)
 console.log (a * b)
 console.log (a / b)
-console.log (a % b)
+console.log (a % b)*/
+
+//segundo exercicio
+
+/*const a = 5
+const b = 4
+
+if (a > b) {
+    console.log (a);
+}
+
+else if (b > a) {
+    console.log(b);
+}
+else {
+    console.log(`A e b são iguais!`);
+}*/
 
