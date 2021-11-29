6063cebc8d22666c721c165be41a466103b1e194
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-29 20:31:32 -0300

    Adiciona até a questão 6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 
 //ex001
 
@@ -15,18 +15,41 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //console.log(result);
  
 //ex003 e 004
-let result = 0;
+//let result = 0;
+//
+//for (index = 0; index < numbers.length; index +=1) {
+//result += numbers[index]
+//}
+//
+//var valorFinal = result / numbers.length
+//
+//console.log(valorFinal)
+//if (valorFinal > 20) {
+//    console.log(`Valor maior que 20`)
+//} else {
+//    console.log(`Valor menor ou igual a 20`);
+//}
 
-for (index = 0; index < numbers.length; index +=1) {
-result += numbers[index]
-}
+//ex005
+//let higherNumber = numbers[0];
+//for (index = 1; index < numbers.length; index +=1) {
+//    if(numbers[index] > higherNumber) {
+//        higherNumber = numbers[index]
+//    }
+//} console.log(higherNumber)
 
-var valorFinal = result / numbers.length
+//ex006
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(valorFinal)
-if (valorFinal > 20) {
-    console.log(`Valor maior que 20`)
+let impares = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !==0) {
+        impares =+ 1;
+    }    
+} 
+if (impares === 0) {
+    console.log(`Nenhum valor foi encontrado`)
 } else {
-    console.log(`Valor menor ou igual a 20`);
+    console.log(impares);
 }
-
