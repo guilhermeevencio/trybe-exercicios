8e8be89ed44f8288e872996f74e0f04cf4290d20
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-29 18:33:50 -0300

    Adiciona a modificacao ao aqruivo script

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -61,8 +61,79 @@ if (a > b && a > c) {
  if ((a + b + c) === 180 ) {
      console.log(`true`);
 
+ }  else if (a <= 0 || b <= 0 || c <= 0) {
+     console.log(`invalido`)
  } else {
     console.log(`false`); 
  }*/
 
- 
+ //exercicio 6
+
+ /*let peca =  `bishop`;
+
+  let pecaminuscula = peca.toLowerCase()
+
+ if (pecaminuscula == `bishop`) {
+     console.log(`diagonals`) ; 
+ } else {
+     console.log(`Peça Inválida`);
+ }*/
+
+ //exercicio 7
+
+ /*let nota = 49
+
+ if (nota >= 90) {
+     console.log(`A`);
+ } else if (nota >= 80) {
+     console.log(`B`)
+ } else if (nota >= 70) {
+     console.log(`C`);
+ } else if (nota >= 60) {
+     console.log(`D`);
+ } else if (nota >= 50) {
+     console.log(`E`);
+ } else {
+     console.log(`F`);
+ }*/
+
+ //exercicio 8
+
+/*const a = 1
+const b = 3
+const c = 5
+
+let par = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0 )) {
+    par = true
+} console.log(par);*/
+
+//exercicio 9
+
+/*const a = 4
+const b = 2
+const c = 6
+
+let odd = false 
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0 )) {
+    odd = true
+} console.log(odd);*/
+
+//exercicio 10
+
+/*const custo = 5 
+const valor = 10 
+let lucro = (valor - (custo + (20 * custo)/100))
+let vendas = 1000
+if (custo <= 0 || valor <= 0) {
+    console.log(`Os valores estáo negativos!`)
+} else {
+console.log(vendas * lucro)
+}*/
+
+//exercicio11
+
+
+
