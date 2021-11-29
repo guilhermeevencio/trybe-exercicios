adc1ccc8bd637018eddfccb6a4253f364d7351f1
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-29 19:13:23 -0300

    adiciona os ex002, ex003 e ex004

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -6,4 +6,27 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //    console.log(numbers[index])
 //}
 
-//
+//ex002
+
+//let result = 0;
+//for (index = 0; index < numbers.length; index +=1) {
+//    result += numbers[index]
+//}
+//console.log(result);
+ 
+//ex003 e 004
+let result = 0;
+
+for (index = 0; index < numbers.length; index +=1) {
+result += numbers[index]
+}
+
+var valorFinal = result / numbers.length
+
+console.log(valorFinal)
+if (valorFinal > 20) {
+    console.log(`Valor maior que 20`)
+} else {
+    console.log(`Valor menor ou igual a 20`);
+}
+
