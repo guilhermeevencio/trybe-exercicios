6b34117530827b36be33a53270d6ca5214c243d4
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-30 18:07:47 -0300

    adiciona os exercicios de javascript a branch exercicios/4.3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,139 @@
+//primeiro exercicio
+
+/*const a = 10
+const b = 2
+console.log (a + b)
+console.log (a - b)
+console.log (a * b)
+console.log (a / b)
+console.log (a % b)*/
+
+//segundo exercicio
+
+/*const a = 5
+const b = 4
+
+if (a > b) {
+    console.log (a);
+}
+
+else if (b > a) {
+    console.log(b);
+}
+else {
+    console.log(`A e b são iguais!`);
+}*/
+
+//terceiro exercicio
+
+/*const a = 1
+const b = 2
+const c = 2
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else if (c > a && c > b) {
+    console.log (c);
+} else {
+    console.log(`eles sao iguais`)
+}*/
+
+//exercicio 4
+
+/*const n = 0
+ if (n > 0) {
+     console.log(`positivo`)
+ } else if (n < 0) {
+    console.log(`negativo`);
+
+ } else {
+     console.log(`zero`);
+ }*/
+
+ //exercicio 5
+
+ /*const a = 28
+ const b = 90
+ const c = 60
+ 
+ if ((a + b + c) === 180 ) {
+     console.log(`true`);
+
+ }  else if (a <= 0 || b <= 0 || c <= 0) {
+     console.log(`invalido`)
+ } else {
+    console.log(`false`); 
+ }*/
+
+ //exercicio 6
+
+ /*let peca =  `bishop`;
+
+  let pecaminuscula = peca.toLowerCase()
+
+ if (pecaminuscula == `bishop`) {
+     console.log(`diagonals`) ; 
+ } else {
+     console.log(`Peça Inválida`);
+ }*/
+
+ //exercicio 7
+
+ /*let nota = 49
+
+ if (nota >= 90) {
+     console.log(`A`);
+ } else if (nota >= 80) {
+     console.log(`B`)
+ } else if (nota >= 70) {
+     console.log(`C`);
+ } else if (nota >= 60) {
+     console.log(`D`);
+ } else if (nota >= 50) {
+     console.log(`E`);
+ } else {
+     console.log(`F`);
+ }*/
+
+ //exercicio 8
+
+/*const a = 1
+const b = 3
+const c = 5
+
+let par = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0 )) {
+    par = true
+} console.log(par);*/
+
+//exercicio 9
+
+/*const a = 4
+const b = 2
+const c = 6
+
+let odd = false 
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0 )) {
+    odd = true
+} console.log(odd);*/
+
+//exercicio 10
+
+/*const custo = 5 
+const valor = 10 
+let lucro = (valor - (custo + (20 * custo)/100))
+let vendas = 1000
+if (custo <= 0 || valor <= 0) {
+    console.log(`Os valores estáo negativos!`)
+} else {
+console.log(vendas * lucro)
+}*/
+
+//exercicio11
+
+
+
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,83 @@
+
+
+//ex001
+
+//for (index = 0 ; index < numbers.length; index +=1) {
+//    console.log(numbers[index])
+//}
+
+//ex002
+
+//let result = 0;
+//for (index = 0; index < numbers.length; index +=1) {
+//    result += numbers[index]
+//}
+//console.log(result);
+ 
+//ex003 e 004
+//let result = 0;
+//
+//for (index = 0; index < numbers.length; index +=1) {
+//result += numbers[index]
+//}
+//
+//var valorFinal = result / numbers.length
+//
+//console.log(valorFinal)
+//if (valorFinal > 20) {
+//    console.log(`Valor maior que 20`)
+//} else {
+//    console.log(`Valor menor ou igual a 20`);
+//}
+
+//ex005
+//let higherNumber = numbers[0];
+//for (index = 1; index < numbers.length; index +=1) {
+//    if(numbers[index] > higherNumber) {
+//        higherNumber = numbers[index]
+//    }
+//} console.log(higherNumber)
+
+//ex006
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let impares = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !==0) {
+        impares += 1;
+    }    
+} 
+if (impares === 0) {
+    console.log(`Nenhum valor foi encontrado`)
+} else {
+    console.log(impares);
+}*/
+
+//ex007
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lowestValue = numbers[0];
+for (index = 1; index < numbers.length; index +=1) {
+    if (lowestValue < numbers[index]) {
+        lowestValue = numbers[index]
+    }
+} console.log(lowestValue)*/
+
+//ex008 e ex009
+
+let n = []
+
+for (index = 1; index <= 25; index += 1) {
+    n.push(index)
+}
+
+
+for (let index = 0; index < n.length; index += 1) {
+    console.log( n[index] / 2)
+    
+
+
+    
+} 
+
