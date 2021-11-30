2db99944e0d3f809022a166660c1cfe796f86d1d
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-30 15:54:12 -0300

    Adiciona o ecxercicio 6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -39,17 +39,17 @@
 //} console.log(higherNumber)
 
 //ex006
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let impares = 0;
 
 for (index = 0; index < numbers.length; index += 1) {
     if (numbers[index] % 2 !==0) {
-        impares =+ 1;
+        impares += 1;
     }    
 } 
 if (impares === 0) {
     console.log(`Nenhum valor foi encontrado`)
 } else {
     console.log(impares);
-}
+}*/
