8be4ed77f0c49f638630d11130db6ac3d0edfbe4
Guilherme <guilhermeevencioluz@gmail.com>
2021-11-30 19:38:53 -0300

    Adiciona o exercicio 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-4_3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-4_3.js
@@ -10,11 +10,22 @@ for (index = 0; index < n; index += 1) {
 
 //exercicio2
 
-let n = 5;
+/*let n = 5;
 let aux = "";
 
 for (index = 0; index < n; index += 1) {
         aux += `*`;
         console.log(aux);
-}
+}*/
 
+//exercicio3
+
+/*let n = 5;
+let aux = "";
+let space = "";
+
+for (index = 0; index < n; index += 1) {
+        space = ` `.repeat(n - index);
+        aux += `*`;
+        console.log(space + aux);
+}*/
