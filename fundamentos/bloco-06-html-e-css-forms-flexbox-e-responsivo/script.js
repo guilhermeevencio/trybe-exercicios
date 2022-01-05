121fe26739e899bda0a50dc901d4115287c8d6ec
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-05 12:48:11 -0300

    adiciona o esboço para resolver as validações que foram neutralizadas depois do prevent.default

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/script.js
@@ -1,4 +1,52 @@
 const state = document.getElementById('state');
+let inputs = {
+    name: {
+        maxlength: 40,
+        required: true,
+    },
+    email: {
+        maxlength: 50,
+        required: true,
+    },
+    cpf: {
+        maxlength: 11,
+        required: true,
+    },
+    adress: {
+        maxlength: 200,
+        required: true,
+    },
+    city: {
+        maxlength: 28,
+        required: true,
+    },
+    state: {
+        type: 'select',
+        required: true,
+    },
+    home: {
+        type: 'radio',
+        required: true,
+    },
+    resumee: {
+        maxlength: 1000,
+        required: true,
+    },
+    job: {
+        maxlength: 40,
+        required: true,
+    },
+    description: {
+        maxlength: 500,
+        required: true,
+    },
+    start: {
+        type: 'date',
+        required: true,
+    },  
+}
+
+
 
 function createOption (e) {
     
@@ -9,4 +57,34 @@ function createOption (e) {
     }
 }
 
-createOption(['AM', 'PI', 'PE', 'PA']);
+
+function manipularSubmits(event){
+event.preventDefault()
+validate();
+}
+
+//validar os dados
+
+function validate() {
+    for (let i in inputs) {
+        let query = `[name=${i}]`;
+        let item = document.querySelector(query);
+        if (item.value !== null) {
+            let content = document.createElement('div');
+            document.body.appendChild(content);
+            let p = document.createElement('p');
+            p.innerText = item.value;
+            content.appendChild(p);
+        } else {
+    window.alert('Preencha os dados!');
+        }
+    }
+}
+
+window.onload = function() {
+    const button = document.getElementById('button-submit');
+    button.addEventListener('click', manipularSubmits);
+
+    createOption(['AM', 'PI', 'PE', 'PA']);
+
+}
