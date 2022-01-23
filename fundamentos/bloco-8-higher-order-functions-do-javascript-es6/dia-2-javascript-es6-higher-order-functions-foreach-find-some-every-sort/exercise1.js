6287398707f7d3ec3f0743e50f948a890427e7d1
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-23 07:49:56 -0300

    adiciona os exercicios do dia 3 do bloco 8

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -0,0 +1,129 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Adicione o código do exercício aqui:
+
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+
+const first1947 = books.find((element) => element.author.birthYear === 1947);
+
+//console.log(first1947);
+
+
+// 2 - Retorne o nome do livro de menor nome.
+
+function smallerName() {
+    let nameBook;
+    books.forEach((element) => {
+        if (!nameBook || element.name.length < nameBook.length) {
+            nameBook = element.name;
+        }
+    });
+    return nameBook;
+}
+
+//console.log(smallerName());
+
+
+// 4 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+const findElement = books.find((element) => element.name.length === 26);
+
+//console.log(findElement);
+
+const ordenar = books.sort((a, b) => a.releaseYear - b.releaseYear);
+
+//console.log(ordenar);
+
+
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+
+const everyoneWasBornOnSecXX = books.every((book) => book.author.birthYear >= 1900 && book.author.birthYear < 2000)
+
+//console.log(everyoneWasBornOnSecXX);
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+
+const someBookWasReleaseOnThe80s = books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
+
+//console.log(someBookWasReleaseOnThe80s);
+
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+
+const authorUnique = books.forEach((book) => {
+    let year = 0;
+    if (book.author.birthYear !== year) {
+        year = book.author.year
+    } 
+    
+    else {return false} 
+})
+
+//c//onsole.log(authorUnique);
+
+let oi = console.log('oi');
+
+console.log(setInterval( oi, 100););
+
+int();
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-Higher Order Functions - map e filter/exercicios.js
@@ -0,0 +1,111 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George. R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Adicione o código do exercício aqui:
+
+  // 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+  const newArr = books.map(({name, genre, author}) => `${name} - ${genre} - ${author.name}`);
+
+//  console.log(newArr);
+
+  // 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+const arrObj = books.map((book) => ({author: book.author.name, age: (book.releaseYear - book.author.birthYear)}));
+
+const sort = arrObj.sort((a, b) => a.age - b.age);
+
+//console.log(sort);
+
+  // 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+const selectedGenres = books.filter(({genre}) => genre === 'Ficção Científica' || genre === 'Fantasia');
+
+//console.log(selectedGenres);
+
+  // 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+
+const oldBooks = books.filter((book) => 2022 - book.releaseYear > 60)
+.sort((a, b) => a.releaseYear - b.releaseYear);
+
+//console.log(oldBooks);
+
+  // 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+
+const arrNames = selectedGenres.map((book) => `${book.author.name}`).sort();
+//console.log(arrNames);
+
+  // 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+  const oldBooks2 = books.filter((book) => 2022 - book.releaseYear > 60);
+
+  const newOld = oldBooks2.map((element) => `${element.name}`);
+
+  //console.log(newOld);
+
+
+  // 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const authorWith3DotsOnName = books.find((book) => book.author.name.split('.').length === 4);
+
+//console.log(authorWith3DotsOnName);
