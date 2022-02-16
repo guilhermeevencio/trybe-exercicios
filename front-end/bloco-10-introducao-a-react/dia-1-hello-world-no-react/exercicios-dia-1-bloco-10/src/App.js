7ea2d744839588b4a926348e96f8d181dbd18e68
Guilherme <guilhermeevencioluz@gmail.com>
2022-02-16 19:25:47 -0300

    Finaliza todos os exercicios e implementa a estilização da página

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-dia-1-bloco-10/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-dia-1-bloco-10/src/App.js
@@ -1,20 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header';
+import Content from './Contents/Contents';
+import Footer from './Footer/Footer';
 
-const Task = (value) => {
 
-  return (
-    <li>{value}</li>
-  );
-}
-const tasks = ['estudar', 'trabalhar', 'fazer exercicios'];
-const ThingsToDo = tasks.map((e) => Task(e));
 function App() {
   return (
     <>
       <Header />
-      { ThingsToDo }
+      <Content />
+      <Footer />
     </>
   );
 }
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-dia-1-bloco-10/src/Contents/Contents.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+
+  const InfoToRender = conteudos.map(({conteudo, bloco, status}) => 
+  <section className="card">
+  <p>{`O conteúdo é: ${conteudo}`}</p>
+  <p>{`Status: ${status}`}</p>
+  <p>{`Bloco: ${bloco}`}</p>
+  </section>)
+
+export default class Content extends Component {
+  render() {
+      return (
+        <div className= 'cards'>
+        {InfoToRender}
+        </div>
+      )
+  }
+}
